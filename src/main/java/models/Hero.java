3913160a0946b48be00b3cd1c5a92c8563d88c22
Steve-njeri <stevenjeri254@gmail.com>
2021-08-06 14:11:25 +0300

    added new members variables to the class hero.java

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
+        private String name;
+        private int age;
+        private String power;
+        private String weakness;
+        private int id;
+        private static ArrayList<Hero> instances = new ArrayList<>();
 }

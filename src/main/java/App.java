3d7a7dfc5a19322c4e69e6dd0c60adbd486ba85c
Steve-njeri <stevenjeri254@gmail.com>
2021-08-08 00:53:03 +0300

    modified app.java , index.hbs, app.css and layout.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,11 +10,11 @@ import java.util.Map;
 public class App {
     public static <hero> void main(String[] args) {
         staticFileLocation("/public");
-
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+        },new HandlebarsTemplateEngine());
+
 
         get("/hero-form",(req, res) ->{
             Map<String, Object> model = new HashMap<>();

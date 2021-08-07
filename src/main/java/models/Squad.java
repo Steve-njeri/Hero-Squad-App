01427ab3f6fdc86ff362060c9c87a71652e111c7
Steve-njeri <stevenjeri254@gmail.com>
2021-08-07 13:58:00 +0300

    added new member variables in the Squad.java class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    private String name;
+    private String cause;
+    private int maxSize;
+    private ArrayList<Hero> hero;
+    private static List<Squad> squadInstances = new ArrayList<>();
 }
